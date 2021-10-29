ab7c24d4d1a3a2638d1128490eeef31d7c4ba7da
Nikolay Avdeev <avdeevklj@mail.ru>
2021-10-30 02:59:30 +0300

    Sprint14Cors

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ const auth = require('./middlewares/auth');
 const NotFoundError = require('./errors/not-found-err');
 const { login, createUser } = require('./controllers/users');
 
+const allowedCors = [
+  'https://praktikum.tk',
+  'http://praktikum.tk',
+  'localhost:3000',
+];
 const PORT = 3000;
 const app = express();
 mongoose.connect('mongodb://localhost:27017/mestodb', {
@@ -17,6 +22,19 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 });
 app.use(express.json());
 app.use(requestLogger);
+app.use((req, res, next) => {
+  const { method } = req;
+  const { origin } = req.headers;
+  const requestHeaders = req.headers['access-control-request-headers'];
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.end();
+  }
+  next();
+});
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
