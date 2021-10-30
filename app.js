56401c5e642498ac55e76e142dc9eb1d1f43c468
Nikolay Avdeev <avdeevklj@mail.ru>
2021-10-30 12:54:21 +0300

    Sprint14Cors3

--- app.js
+++ app.js
@@ -27,9 +27,9 @@ app.use((req, res, next) => {
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', '*');
   }
-  const requestHeaders = req.headers['access-control-request-headers'];
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     return res.end();
   }
   next();
